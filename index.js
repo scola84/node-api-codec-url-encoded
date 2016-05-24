3b5e0c42cdc2669576df7255678689cb5f88963c
M.C. van den Boogaart <scola@kvdb.name>
2016-05-24 14:00:12 +0200

    Use factory methods

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const Decoder = require('./lib/decoder');
+const Encoder = require('./lib/encoder');
+const type = 'application/x-www-form-urlencoded';
+
+function decoder(options) {
+  return {
+    type,
+    create: () => new Decoder(options)
+  };
+}
+
+function encoder(options) {
+  return {
+    type,
+    create: () => new Encoder(options)
+  };
+}
+
 module.exports = {
-  type: 'application/x-www-form-urlencoded',
-  Decoder: require('./lib/decoder'),
-  Encoder: require('./lib/encoder')
+  type,
+  decoder,
+  encoder,
+  Decoder,
+  Encoder
 };
